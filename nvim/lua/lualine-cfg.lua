6672fc0a33c2fe2836feaa6bfe49cb47bd62d457
syltr1x <syltr1x@proton.me>
2024-09-19 22:31:28 -0300

    change: neovim lualine config

--- nvim/lua/lualine-cfg.lua
+++ nvim/lua/lualine-cfg.lua
@@ -1,14 +1,31 @@
-local function get_lsp_diagnostics()
-  local error_count = vim.lsp.diagnostic.get_count(0, "Error")
-  local warning_count = vim.lsp.diagnostic.get_count(0, "Warning")
+local function get_colored_diagnostics()
+  local diagnostics = {}
+  local error_count = #vim.diagnostic.get(0, {severity = vim.diagnostic.severity.ERROR})
+  local warning_count = #vim.diagnostic.get(0, {severity = vim.diagnostic.severity.WARN})
+  local hint_count = #vim.diagnostic.get(0, {severity = vim.diagnostic.severity.HINT})
 
-  return string.format("E:%d W:%d", error_count, warning_count)
+  if error_count > 0 then
+    table.insert(diagnostics, string.format("%s:%d", "%#LspDiagnosticsError#", error_count))
+  end
+  if warning_count > 0 then
+    table.insert(diagnostics, string.format("%s󱈸:%d", "%#LspDiagnosticsWarning#", warning_count))
+  end
+  if hint_count > 0 then
+    table.insert(diagnostics, string.format("%s:%d", "%#LspDiagnosticsHint#", hint_count))
+  end
+
+  return table.concat(diagnostics, ' ')
 end
+
+-- Define highlight groups
+vim.cmd([[highlight LspDiagnosticsError guifg=#ec5f67 guibg=#403c3a]])
+vim.cmd([[highlight LspDiagnosticsWarning guifg=#ECBE7B guibg=#403c3a]])
+vim.cmd([[highlight LspDiagnosticsHint guifg=#548abf guibg=#403c3a]])
 
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox', -- Cambia a tu tema preferido
+    theme = 'gruvbox', -- Bar theme, see more in https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
   },
@@ -26,10 +43,11 @@ require('lualine').setup {
         },
         symbols = {added = ' ', modified = '柳 ', removed = ' '},
         source = nil,
-      }
+      },
+      get_colored_diagnostics,
     },
     lualine_c = {'filename'},
-    lualine_x = {get_lsp_diagnostics, 'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
